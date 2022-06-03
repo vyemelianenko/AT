cb7b6081b0acabefdf008890cd1f22a88f586d26
Viktor Yemelianenko <vyemelianenko@provectus.com>
2022-06-03 18:19:35 +0300

    Initial commit

---
+++ src/com/provectus/session2/MyFirstClass.java
@@ -0,0 +1,12 @@
+package com.provectus.session2;
+
+/**
+ * @author Victor Yemelianenko vemelianenko@model.com
+ */
+
+public class MyFirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
