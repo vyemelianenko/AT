99876c8920c10543eb684757f525365ab37bcca4
Viktor Yemelianenko <vyemelianenko@provectus.com>
2022-06-08 12:52:18 +0300

    justPush

---
+++ src/com/provectus/session3/Condition.java
@@ -0,0 +1,27 @@
+package com.provectus.session3;
+
+import java.util.Locale;
+
+/**
+ * @author Victor Yemelianenko vemelianenko@model.com
+ */
+
+public class Condition {
+    public static void main(String[] args) {
+
+        String str1 = "Some Text Today";
+        String str2 = null;
+
+        String result1 = str1.toUpperCase();
+        String result2 = str1.toLowerCase();
+
+        System.out.println(result1);
+        System.out.println(result2);
+
+//        String formatted = String.format("result of %s and %s",result1,result2);
+        String formatted = String.format("result of %s and %d %f",result1 , 5,5.6);
+
+        System.out.println(formatted);
+    }
+
+}
