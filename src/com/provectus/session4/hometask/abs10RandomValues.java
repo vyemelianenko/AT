e1e50152c11cc6e4b7ae08845dd1dd02bc9a7e61
Viktor Yemelianenko <vyemelianenko@provectus.com>
2022-06-13 22:13:50 +0300

    New HomeTask is ready for verify version

--- src/com/provectus/session4/hometask/abs10RandomValues.java
+++ src/com/provectus/session4/hometask/abs10RandomValues.java
@@ -14,7 +14,7 @@ public class abs10RandomValues {
         for (int i = 0; i < randomArray.length; i++) {
             int j = random.nextInt(100);
             sumAllValues += j;
-            System.out.println("Each random element bu index " + i + " --> " + j);
+            System.out.println("Each random element by index " + i + " --> " + j);
             // System.out.println("sum tep by step " + sumAllValues);
         }
         System.out.println("This is sum of all values from randomArray " + sumAllValues / randomArray.length);
